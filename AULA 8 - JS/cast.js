32c829be5efd5ccd82f0d11e7b11c0e5b8c0ac33
Rafael Walker Batista <rafaelwalkerbatista@gmail.com>
2021-03-07 22:44:35 -0300

    aula 8

---
+++ AULA 8 - JS/cast.js
@@ -0,0 +1,3 @@
+let numero = 20;
+numero = String(numero);
+console.log(typeof numero);
---
+++ AULA 8 - JS/types.js
@@ -0,0 +1,13 @@
+let carro = "Ferrari";
+const nome = "Rafael";
+var num = 2021;
+let bolMasc = true;
+let diverso = null;
+let diverso2;
+
+console.log(typeof carro);
+console.log(typeof nome);
+console.log(typeof ano);
+console.log(typeof bolMasc);
+console.log(typeof diverso);
+console.log(typeof diverso2);
